e74d40ec292bc4eb049b03ae9b05af35197ac743
Raphael Catolino <raphael.catolino@gmail.com>
2016-09-17 21:21:39 +0200

    move plot management into separate functions

--- control.py
+++ control.py
@@ -11,9 +11,10 @@ from numpy import fft
 
 
 def bandstop(transform, bandwidth=3):
-    ftransform = transform
+    ftransform = numpy.copy(transform)
     ftransform[0] = 0
     ftransform[1] = 0
+    ftransform[2] = 0
     ftransform[-1] = 0
     return ftransform
 
@@ -55,93 +56,108 @@ class pid_controller:
             print("error {}, p {}, i {}, d {}".format(error, p, i, d))
         return (p + i + d, p, i, d)
 
-class ksp:
-    def __init__(self, game_address="128.0.0.1", resolution=100):
-        self.client = krpc.connect(name="ap", address=game_address)
+class autopilot:
+    def __init__(self, remote_address="128.0.0.1", resolution=100, pitch_target=0):
+        self.client = krpc.connect(name="ap", address=remote_address)
         print("Connected to {}".format(self.client.krpc.get_status()))
         self.vessel = self.client.space_center.active_vessel
         print("Controling {}".format(self.vessel.name))
+
         self.position = self.vessel.flight(self.vessel.orbit.body.reference_frame)
         self.attitude = self.vessel.flight(self.vessel.surface_reference_frame)
         self.control = self.vessel.control
+
         self.time_interval = resolution # time between updates in ms
-        self.pitch_controller = pid_controller(6/90, 3.5, 0.3, 0.01, self.time_interval/1000)
-        self.roll_controller = pid_controller(0, 0.4, 0.1, 0.05, self.time_interval/1000)
-        #self.alt_controller = pid_controller(18000/25000, 3.5, 0.1, 0.55, 0.1, max_pitch=25)
-        self.data_history = []
-        self.pv_correction = 0
+        self.pv_smoothed = 0
+        self.pitch_target = pitch_target
+
         datapoint_nb = int(5*1000/self.time_interval) # holds 5*1000ms worth of data
+        self.history = deque(maxlen=datapoint_nb)
+        self.history.append(0)
+        self.data_history = []
         for i in range(0, 5):
             self.data_history.append(deque(maxlen=datapoint_nb))
             self.data_history[i].append(0)
 
-        self.history = deque(maxlen=datapoint_nb)
-        self.history.append(0)
+        self.pitch_controller = pid_controller(self.pitch_target/90, 3.5, 0.3, 0.01, self.time_interval/1000)
+        self.roll_controller = pid_controller(0, 0.4, 0.1, 0.05, self.time_interval/1000)
 
-    def plot_update(self, frame, axes, control_plot, parameter_plot, pv_transform_plot, cv_transform_plot):
-        #cr = pos_ctrl.vertical_speed
-        #alt = p_flight.mean_altitude
-        #print("climbing rate : {}\taltitude : {}".format(cr, alt))
+    def update_callback(self, frame, axes, control_plot, process_plot, pv_freq_plot, pv_filtered_plot):
         attitude = self.attitude
-        apitch = attitude.pitch
-        #print("pitch : {}\theading : {}\troll: {}\tAoA : {}".format(apitch, attitude.heading, attitude.roll, attitude.angle_of_attack))
-        #c.pitch = alt_controller.control(p_flight.mean_altitude/25000, log=True, pitch=attitude.pitch)
-        self.control.roll = self.roll_controller.control(attitude.roll/90)[0]
-        print("correction {}, pitch {}, pitch corrected {}".format(self.pv_correction, apitch, apitch - self.pv_correction))
-        (pitch, p, i, d) = self.pitch_controller.control((apitch - self.pv_correction)/90)
-        self.control.pitch = pitch
+        pitch = attitude.pitch
+        print("pitch : {}\theading : {}\troll: {}\tAoA : {}".format(pitch, attitude.heading, attitude.roll, attitude.angle_of_attack))
 
-        self.data_history[0].append(pitch)
+        self.control.roll = self.roll_controller.control(attitude.roll/90)[0]
+        if self.pv_smoothed != 0:
+            print("last process pitch {}, corrected pitch {}".format(self.data_history[4][-1], self.pv_smoothed))
+            process_pitch = self.pv_smoothed
+        else:
+            process_pitch = self.pitch_target
+
+        (control_pitch, p, i, d) = self.pitch_controller.control(process_pitch/90)
+        self.control.pitch = control_pitch
+
+        pv_fft, pv_filtered, pv_noise = self.update_flight_data(pitch, control_pitch, p, i, d)
+        return self.update_plots(axes, control_plot, process_plot, pv_freq_plot, pv_filtered_plot, pv_fft, pv_filtered, pv_noise)
+
+    def update_flight_data(self, process_var, control_var, p, i, d):
+        self.data_history[0].append(control_var)
         self.data_history[1].append(p)
         self.data_history[2].append(i)
         self.data_history[3].append(d)
-        self.data_history[4].append(apitch)
+        self.data_history[4].append(process_var)
         self.history.append(self.history[-1] + self.time_interval/1000)
 
+        if len(self.history) > 6:
+            pv_fft = fft.rfft(self.data_history[4])
+            pv_filtered = bandstop(pv_fft)
+            pv_noise = fft.irfft(pv_filtered, len(self.history))
+            #self.pv_noise_correction = 2*pv_noise[-1] - pv_noise[-2] # crude estimation of the next value for the process variable noise
+            self.pv_smoothed = self.data_history[4][-1] - pv_noise[-1] # process variable with estimated noise removed
+        else:
+            pv_fft = []
+            pv_filtered = []
+            pv_noise = []
+        return pv_fft, pv_filtered, pv_noise
+
+    def update_plots(self, control_axes, control, process, frequency, frequency_filtered, pv_fft, pv_filtered, pv_noise):
+        process[0].set_data(self.history, self.data_history[4])
+        control_axes.set_xlim(self.history[0], max(5, self.history[-1]))
         for i in range(0, len(self.data_history)-1):
-            control_plot[i].set_data(self.history, self.data_history[i])
+            control[i].set_data(self.history, self.data_history[i])
 
-        parameter_plot[0].set_data(self.history, self.data_history[4])
-        axes.set_xlim(self.history[0], max(5, self.history[-1]))
+        if len(pv_fft) > 0:
+            freqs = fft.fftshift(fft.fftfreq(len(pv_fft)))
+            frequency[0].set_data(freqs, numpy.abs(fft.fftshift(pv_fft)))
+            frequency_filtered[0].set_data(freqs, numpy.abs(fft.fftshift(pv_filtered)))
 
-        if len(self.history) > 10:
-            pv_transform = fft.rfft(self.data_history[4])
-            cv_transform = fft.rfft(self.data_history[0])
-            freqs = fft.fftshift(fft.fftfreq(len(cv_transform)))
-            pv_transform_plot[0].set_data(freqs, numpy.abs(fft.fftshift(pv_transform)))
-            cv_transform_plot[0].set_data(freqs, numpy.abs(fft.fftshift(bandstop(pv_transform))))
+            process[1].set_data(self.history, pv_noise)
+            process[2].set_data(self.history, self.data_history[4] - pv_noise)
 
-            ipv_transform = fft.irfft(bandstop(pv_transform), len(self.history))
-            parameter_plot[1].set_data(list(self.history)[0:len(ipv_transform)], ipv_transform)
-            parameter_plot[2].set_data(self.history, self.data_history[4] - ipv_transform)
-            print("last itransform {}, last pitch {}, diff {}".format(ipv_transform[-1], apitch, apitch - ipv_transform[-1]))
-            self.pv_correction = 2*ipv_transform[-1] - ipv_transform[-2]
-
-        return (axes, control_plot, parameter_plot, pv_transform_plot, cv_transform_plot)
+        return (control_axes, control, process, frequency, frequency_filtered)
 
     def run_ap(self):
         sas_state = self.control.sas
         self.control.sas = False
         fig = pyplot.figure()
         control_axes = fig.add_subplot(2,2,1)
-        parameter_axes = fig.add_subplot(2,2,3, sharex=control_axes)
-        pv_transform_axes = fig.add_subplot(2,2,2)
-        cv_transform_axes = fig.add_subplot(2,2,4, sharex=pv_transform_axes)
+        process_axes = fig.add_subplot(2,2,3, sharex=control_axes)
+        pv_freq_axes = fig.add_subplot(2,2,2)
+        pv_filtered_axes = fig.add_subplot(2,2,4, sharex=pv_freq_axes, sharey=pv_freq_axes)
 
         control_axes.set_xlim(0, 5)
         control_axes.set_ylim(-1.5, 1.5)
-        parameter_axes.set_ylim(-40, 40)
-        pv_transform_axes.set_xlim(-5, 5)
-        pv_transform_axes.set_ylim(0, 80)
-        cv_transform_axes.set_ylim(0, 50)
+        process_axes.set_ylim(-40, 40)
+        pv_freq_axes.set_xlim(-5, 5)
+        pv_freq_axes.set_ylim(0, 80)
 
         control_plot = control_axes.plot([], [], 'b', [], [], 'c', [], [], 'r', [], [], 'm')
-        parameter_plot = parameter_axes.plot([], [], 'b', [], [], 'r', [], [], 'm')
-        pv_transform_plot = pv_transform_axes.plot([], [])
-        cv_transform_plot = cv_transform_axes.plot([], [])
-        control_axes.legend(control_plot, ['pitch input', 'p value', 'i value', 'd value'], loc=3)
-        parameter_axes.legend(parameter_plot, ['pitch', 'pitch noise', 'filtered pitch'], loc=3)
-        anim = animation.FuncAnimation(fig, self.plot_update, fargs=(control_axes, control_plot, parameter_plot, pv_transform_plot, cv_transform_plot), interval=self.time_interval) # a frame every time_interval
+        process_plot = process_axes.plot([], [], 'b', [], [], 'r', [], [], 'm')
+        pv_freq_plot = pv_freq_axes.plot([], [])
+        pv_filtered_plot = pv_filtered_axes.plot([], [])
+        control_axes.legend(control_plot, ['pitch control', 'p value', 'i value', 'd value'], loc=3)
+        process_axes.legend(process_plot, ['pitch', 'pitch noise', 'filtered pitch'], loc=3)
+        anim = animation.FuncAnimation(fig, self.update_callback, fargs=(control_axes, control_plot, process_plot, pv_freq_plot, pv_filtered_plot), interval=self.time_interval)
         try:
             pyplot.show()
         except KeyboardInterrupt:
@@ -150,5 +166,5 @@ class ksp:
             self.control.sas = sas_state
 
 
-game = ksp("192.168.2.3")
-game.run_ap()
+ap = autopilot(remote_address="192.168.2.3", pitch_target=4)
+ap.run_ap()
